c074b2c044417e050dc18124467e3d0413ebf5da
Louis Rodriguez <louisp285@yahoo.com>
2019-06-13 00:56:46 -0500

    set up server

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
 USE burgers_db;
 INSERT INTO burgers(burger_name, devoured)
+
+    -- using these as examples to practice with --
 VALUES ("hamburger", false), ("cheeseburger", false), ("veggieburger", false);
---
+++ server.js
@@ -0,0 +1,32 @@
+    // required dependencies //
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+    // app variable to run express //
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+    // static files //
+app.use(express.static("public"));
+
+    // body parser elements for url and json //
+    // to handle the HTTP POST, this midlleware is needed.
+    // if not used, you have to individually manipulate all of the fields //
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+    // setting up handlebars //
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+    // these are the routes we will be using //
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+    // confirming connection with server //
+app.listen(PORT, function() {
+    console.log("server is listening on: http://localhost:" + PORT);
+});
+
+
