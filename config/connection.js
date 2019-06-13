d5436e3b087445672e619f4bd14741b662ba396b
Louis Rodriguez <louisp285@yahoo.com>
2019-06-13 01:31:11 -0500

    connection set

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+    // requiring dependency //
+var mysql = require("mysql");
+
+    // create the connection to the database // 
+connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "smu2019",
+    database: "burgers_db"
+});
+
+    // this will let me know if this is connected //
+connection.connect(function(err) {
+    if(err) {
+        console.log ("not connected: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+    // exporting the module //
+module.exports = connection; 
